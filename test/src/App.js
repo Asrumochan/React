36de8d2d23531de1b46e6392d3dc4b44836f16ed
Asrumochan <asrumochanparida99@gmail.com>
2023-11-16 22:25:42 +0530

    16-11-23

--- test/src/App.js
+++ test/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from "./components/list_table/navbar"
-import Test from "./components/API/digitalClock/digitalColock"
+import Test from "./components/API/test/test"
 function App() {
   return (
     <div>
