c2530379e8822659b2a48b1e782844bfb71d18dd
Asrumochan <asrumochanparida99@gmail.com>
2023-11-17 22:42:29 +0530

    17-11-23

--- test/src/components/Routing/navbar.jsx
+++ test/src/components/Routing/navbar.jsx
@@ -7,10 +7,10 @@ export class navbar extends Component {
       <Link to="/home" className='navbar-brand'>Company Logo</Link>
       <div className='ml-auto'>
          <ul className='navbar-nav'>
-              <li><Link to="/home" className='nav-link'>Home</Link></li>
-              <li><Link to="/about"  className='nav-link'>About</Link></li>
-              <li><Link to="/contact"  className='nav-link'>Contact Us</Link></li>
-              <li><Link to="/login"  className='nav-link'>Login</Link></li>
+              <li><Link to="./home" className='nav-link'>Home</Link></li>
+              <li><Link to="./about"  className='nav-link'>About</Link></li>
+              <li><Link to="./contact"  className='nav-link'>Contact Us</Link></li>
+              <li><Link to="./login"  className='nav-link'>Login</Link></li>
          </ul>
       </div>
  </nav> 
