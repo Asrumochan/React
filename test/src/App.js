eac98dd4336d1cac8e8f7a9bc8052e51b4294895
Asrumochan <asrumochanparida99@gmail.com>
2023-11-17 22:00:05 +0530

    17-11-23

--- test/src/App.js
+++ test/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Navbar from "./components/list_table/navbar"
-import Test from "./components/API/test/test"
 function App() {
   return (
     <div>
       <Navbar/>
-      <Test/>
     </div>
   )
 }
