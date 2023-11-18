9ae2c7d06c296f6c3a14bc349c58a5f6885ff03a
Asrumochan <asrumochanparida99@gmail.com>
2023-11-18 23:55:23 +0530

    18-11-23

---
+++ test/src/components/Routing/appp.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from "./components/Routing/navbar"
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Home from './components/Routing/home'
+import About from './components/Routing/about'
+import Contact from './components/Routing/contact'
+import Login from './components/Routing/login'
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar/>
+        <Routes>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
