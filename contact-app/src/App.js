3df7d0929211db01db83f6337d16b2c0ca276cd2
Asrumochan <asrumochanparida99@gmail.com>
2024-03-14 17:03:24 +0530

    sdfx

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,8 +1,13 @@
  import Header from "./components/Header";
+ import AddContactList from "./components/AddContact";
+ import ContactList from "./components/ContactList";
+
 function App() {
   return (
     <>
     <Header/>
+    <AddContactList/>
+    <ContactList/>
     </>
   );
 }
--- contact-app/src/components/AddContact.jsx
+++ contact-app/src/components/AddContact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ContactList = () => {
+const AddContact = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const ContactList = () => {
   )
 }
 
-export default ContactList
+export default AddContact
