436d4751db1b7e794742216b44f82766e20370b9
Asrumochan <asrumochanparida99@gmail.com>
2024-03-20 20:59:20 +0530

    we

--- test/src/App.js
+++ test/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Useeffect from './components/Hooks/useeffect'
+import Usestate from './components/Hooks/usestate'
 function App() {
+  const [flag ,setFlag]=useState(true)
+  const changeFlag=()=>{
+    setFlag(!flag)
+  }
   return (
     <div>
-      <Useeffect/>
+      {
+        flag ? <Useeffect/> : <Usestate/>
+      }
+       <button onClick={changeFlag}>Change </button>
     </div>
   )
 }
