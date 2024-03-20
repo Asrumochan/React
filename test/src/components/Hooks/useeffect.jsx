b996b9d6ba7ab4bdc69eb210bbf8ec189b929be3
Asrumochan <asrumochanparida99@gmail.com>
2024-03-20 22:40:51 +0530

    hguh

--- test/src/components/Hooks/useeffect.jsx
+++ test/src/components/Hooks/useeffect.jsx
@@ -6,19 +6,24 @@ const Hook =()=>{
     const [val,setVal]=useState([])
     const [counter,setCounter]=useState(0)
 
+
     useEffect(()=>{
         console.log("mounted ")
-        Axios.get("https://jsonplaceholder.typicode.com/users")
-        .then((resp)=>{
-            setVal(resp.data)
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
+    },[])
+    useEffect(()=>{
         return ()=>{
             console.log("effect changed")  
         }
-    },[counter])
+    },[])
+    useEffect(()=>{
+        Axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((resp)=>{
+            setVal(resp.data)
+            console.log("state changing")
+        })
+        .catch((err)=>{
+            console.log(err)
+        })},[counter]);
     const getData=()=>{
         setCounter((c)=>c+1);
         }
@@ -26,10 +31,10 @@ const Hook =()=>{
     return(
         <>
         <h1>Hooks</h1>
-        <h1 onClick={getData}>{counter}</h1>
+        <h1 onClick={getData}>{counter} +</h1>
         {
             val.map((user)=>(
-                <h4>{user.name}</h4>
+                <h4 key={user.id}>{user.name}</h4>
             ))
         }
         
