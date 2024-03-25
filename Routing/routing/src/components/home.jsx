69054057c96d9f4f824bb8671f2c39f9dcca7471
Asrumochan <asrumochanparida99@gmail.com>
2024-03-25 18:25:08 +0530

    saf

--- Routing/routing/src/components/home.jsx
+++ Routing/routing/src/components/home.jsx
@@ -3,21 +3,18 @@ import React,{Component} from 'react'
 const users=[{id:1,name: 'user 1'}, {id:2,name:'user 2'}, {id:3,name:'user 3'}]
 class Home extends Component{
   render(){
-
+    console.log(this.props)
       return (
-          <>
-                        <ul>
-                            
-                    {
-
-                        users.map((user) =>(
-                            <li key={user.id} >
-                                <Link to={`/user/${user.id}`} >{user.name}</Link>
-                            </li>
-                            ))
-                            
-                    }
-                    </ul>
+          <><ul>          
+            {
+                users.map((user) =>(
+                    <li key={user.id} >
+                        <Link to={`/user/${user.id}`} >{user.name}</Link>
+                    </li>
+                    ))
+                    
+            }
+            </ul>
            </>
   )
 }
--- Routing/routing/src/components/user.jsx
+++ Routing/routing/src/components/user.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
-import { useNavigate } from 'react-router-dom';
 const User = (props) => {
-  const history =useNavigate()
   const params = useParams()
-  console.log(history)
   return (
     <div>
         <h1></h1>
